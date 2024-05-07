9077d825680d60387da7bb8fd29a15649c04fef7
saba <sabameshveliani2008@icloud.com>
2024-05-08 00:31:05 +0400

    axali kodi modis mesamed

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,51 +1,51 @@
 
-"use client"
-import React, { useState } from 'react';
-import { Alert, Calendar } from 'antd';
-import dayjs, { Dayjs } from 'dayjs';
-import axios from 'axios';
+// "use client"
+// import React, { useState } from 'react';
+// import { Alert, Calendar } from 'antd';
+// import dayjs, { Dayjs } from 'dayjs';
+// import axios from 'axios';
 
-export default function YourComponent() {
-  const [value, setValue] = useState<Dayjs>(() => {
-    const storedValue = localStorage.getItem('ახალი ჯავშანი');
-    return storedValue ? dayjs(storedValue) : dayjs('2017-01-25');
-  });
+// export default function YourComponent() {
+//   const [value, setValue] = useState<Dayjs>(() => {
+//     const storedValue = localStorage.getItem('ახალი ჯავშანი');
+//     return storedValue ? dayjs(storedValue) : dayjs('2017-01-25');
+//   });
 
-  const onSelect = (newValue: Dayjs) => {
-    setValue(newValue);
-    localStorage.setItem('ახალი ჯავშანი', newValue.format());
-  };
+//   const onSelect = (newValue: Dayjs) => {
+//     setValue(newValue);
+//     localStorage.setItem('ახალი ჯავშანი', newValue.format());
+//   };
 
-  const onPanelChange = (newValue: Dayjs) => {
-    setValue(newValue);
-    localStorage.setItem('ახალი ჯავშანი', newValue.format());
-  };
-//   const click = async() =>{
-//     fetch('http://localhost:3001/login', {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({ name:value })
-// })
-//     .then((res) => {
-//         return res.json();
-//     })
-//     .then((response) => {
-//         console.log(response);
-//     })
-//     .catch((error) => {
-//         console.error(error.message);
-//     });
-//   }
+//   const onPanelChange = (newValue: Dayjs) => {
+//     setValue(newValue);
+//     localStorage.setItem('ახალი ჯავშანი', newValue.format());
+//   };
+// //   const click = async() =>{
+// //     fetch('http://localhost:3001/login', {
+// //     method: 'POST',
+// //     headers: {
+// //         'Content-Type': 'application/json'
+// //     },
+// //     body: JSON.stringify({ name:value })
+// // })
+// //     .then((res) => {
+// //         return res.json();
+// //     })
+// //     .then((response) => {
+// //         console.log(response);
+// //     })
+// //     .catch((error) => {
+// //         console.error(error.message);
+// //     });
+// //   }
 
 
   
-  return (
-    <>
-      <Alert message={`თქვენ დაჯავშნეთ ლექცია: ${value?.format('YYYY-MM-DD')}`} />
-      <Calendar value={value} onSelect={onSelect} onPanelChange={onPanelChange} />
+//   return (
+//     <>
+//       <Alert message={`თქვენ დაჯავშნეთ ლექცია: ${value?.format('YYYY-MM-DD')}`} />
+//       <Calendar value={value} onSelect={onSelect} onPanelChange={onPanelChange} />
 
-    </>
-  );
-}
+//     </>
+//   );
+// }
