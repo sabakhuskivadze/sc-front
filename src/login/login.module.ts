acf08198cb50b330efdd6b44989e2ad23ef79212
saba <sabameshveliani2008@icloud.com>
2024-05-07 21:51:05 +0400

    sdasddsadasdadasdadasdasda

--- src/login/login.module.ts
+++ src/login/login.module.ts
@@ -11,20 +11,12 @@ import { jwtConstants } from './jwt.token.secret';
     MongooseModule.forFeature([{name: 'login', schema:loginSchema}]),
     JwtModule.register({
       secret: 'your_secret_key_here',
-<<<<<<< HEAD
       signOptions: { expiresIn: '24h' },
-=======
-      signOptions: { expiresIn: '24h' }, 
->>>>>>> fc343b6a99360f5d5405b717aaa378d3a6c6e797
     }),
   ],
   controllers: [LoginController],
   providers: [LoginService]
 })
 export class LoginModule {}
-<<<<<<< HEAD
    
 //ds
-=======
-   
->>>>>>> fc343b6a99360f5d5405b717aaa378d3a6c6e797
